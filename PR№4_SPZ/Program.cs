940c969c14b7627557fb9556e82441630851d1e0
Anastasia Lyah <lyahanastasya@gmail.com>
2024-05-05 22:17:37 +0300

    Add project files.

---
+++ PR№4_SPZ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Town
+{
+    public string Name; 
+    public string Country; 
+    public string Region; 
+    public int Population; 
+    public double YearIncome; 
+    public double Square; 
+    public bool HasPort;
+    public bool HasAirport;
+    public double GetYearIncomePerInhabitant()
+    {
+        return YearIncome / Population;
+    }
+}
